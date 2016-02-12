00a41df6c4c333ed7e4ecde104efc8db4a821de4
jasonunger92 <jasonscottunger@gmail.com>
2016-02-12 14:25:20 -0500

    Guessing Game updated

--- guessGame.js
+++ guessGame.js
@@ -1,14 +1,3 @@
-/* **** Global Variables **** */
-// try to elminate these global variables in your project, these are here just to start.
-
-var playersGuess,
-winningNumber,
-numGuessesLeft = 5,
-prevGuesses = [],
-hintGiven = false;
-
-
-
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
@@ -22,10 +11,71 @@ function generateWinningNumber(){
 function playersGuessSubmission(){
   playersGuess = +document.getElementById("guess").value || null;
   document.getElementById("guess").value = "";
-  guessesLeft();
   prevGuesses.push(playersGuess);
+  guessesLeft();
   checkGuess();
   showLastGuess();
+}
+
+// Check if the Player's Guess is the winning number 
+
+function checkGuess(){
+  if (playersGuess === winningNumber) {
+    $("#info").html("You guessed the number!<br><br>It was ".toUpperCase() + winningNumber + "!").show();
+    $("#picture").html("<img class=\"pic\" src=\"images/happy_jordan.png\" style=\"width: 400px\">");
+    $("#info").addClass("winner");
+    gameEnd();
+  } else if (playersGuess === null || playersGuess < 1 || playersGuess > 100 || isNaN(playersGuess)) {
+    $("#info").html("Please enter a valid number").show();
+    reset();
+  } else if (checkDuplicate() === true) {
+    $("#info").html("You guessed that number already...").show();
+    reset();
+  } else {
+    guessMessage();
+  };
+}
+
+// Reset guesses after invalid guess
+
+function reset() {
+  prevGuesses.pop();
+  numGuessesLeft++;
+  $("#helper").html("You have " + numGuessesLeft + " more guesses!").show();
+}
+
+// Check to see if Player is guessing a number previously guessed
+
+function checkDuplicate() {
+  for(var i = 0; i < prevGuesses.length-1; i++) {
+    if (playersGuess === prevGuesses[i]) {
+      return true;
+    };
+  };
+}
+
+// Inform Player of how they are doing
+
+function guessMessage() {
+  if (numGuessesLeft > 0) {
+    var direction = lowerOrHigher();
+    var absolute = Math.abs(playersGuess - winningNumber);
+    var distance = "";
+    if (absolute > 20) {
+      distance = "<span class=\"freezing\" style=\"color:#86C7FF\">Freezing</span>";
+    } else if (absolute > 10) {
+      distance = "<span class=\"cold\" style=\"color:#1D7EFF\">Cold</span>";
+    } else if (absolute < 5) {
+      distance = "<span class=\"hot\" style=\"color:#DC0000\">Hot</span>";
+    } else {
+      distance = "<span class=\"warm\" style=\"color:#FF7A15\">Warm</span>";
+    };
+    $("#info").html("Your Guess is <strong>" + direction.toUpperCase() + "</strong> and <strong>" + distance.toUpperCase() + "</strong>").show();
+  } else {
+    $("#info").addClass("loser");
+    $("#info").html("Sorry... The number was ".toLowerCase() + winningNumber).show();
+    $("#picture").html("<img class=\"pic\" src=\"images/sad_jordan.png\" style=\"width: 400px\">");
+  };
 }
 
 // Determine if the next guess should be a lower or higher number
@@ -38,26 +88,27 @@ function lowerOrHigher(){
   };
 }
 
-// Check if the Player's Guess is the winning number 
+// Keep track of guesses remaining
 
-function checkGuess(){
-  if (playersGuess === null || playersGuess < 1 || playersGuess > 100 || isNaN(playersGuess)) {
-    $("#info").html("Please enter a valid number").show();
-    prevGuesses.pop();
-    numGuessesLeft++;
-    $("#helper").html("You have " + numGuessesLeft + " more guesses!").show()
-  } else if (playersGuess === winningNumber) {
-    $("#info").html("You guessed the number!".toUpperCase()).show();
-    $("h3#info").addClass("winning");
-    $("#helper").hide();
-    $("body").addClass("winner");
-  } else if (checkDuplicate() === true) {
-    $("#info").html("You guessed that number already...").show();
-    prevGuesses.pop();
-    numGuessesLeft++;
-    $("#helper").html("You have " + numGuessesLeft + " more guesses!").show()
+function guessesLeft() {
+  if (numGuessesLeft > 2) {
+    numGuessesLeft--;
+    $("#helper").html("You have " + numGuessesLeft + " more guesses!").show();
+  } else if (numGuessesLeft === 2) {
+    numGuessesLeft--;
+    $("#helper").html("You have " + numGuessesLeft + " more guesses!").show();
   } else {
-    guessMessage();
+    numGuessesLeft--;
+    $("last-guess").hide();
+    gameEnd();
+  };
+}
+
+// Display the last guess to the Player
+
+function showLastGuess() {
+  if (prevGuesses.length > 0 && prevGuesses.length < 5 && playersGuess != winningNumber) {
+    $('#last-guess').html("Your last guess was: " + prevGuesses[prevGuesses.length - 1]).show();
   };
 }
 
@@ -82,63 +133,7 @@ function provideHint(){
   };
 }
 
-// Allow the "Player" to Play Again
-
-function playAgain(){
-  generateWinningNumber();
-  prevGuesses = [];
-  numGuessesLeft = 5;
-  hintGiven = false;
-  $("#helper").html("You have " + numGuessesLeft + " more guesses!").show()
-  $("#info").hide();
-  $("#lastGuess").hide();
-  $("body").removeClass("winner");
-  $("h3#info").removeClass("winning");
-}
-
-function guessesLeft() {
-  numGuessesLeft > 2 ? (numGuessesLeft--, $("#helper").html("You have " + numGuessesLeft + " more guesses!").show()) : 2 === numGuessesLeft ? (numGuessesLeft--, $("#helper").html("You have " + numGuessesLeft + " more guess!").show()) : $("#helper").html("Sorry, Play Again!").show()
-}
-
-function checkDuplicate() {
-  var test = 0;
-  for(var i = 0; i < prevGuesses.length-1; i++) {
-    if (playersGuess === prevGuesses[i]) {
-      test++;
-    };
-  };
-  if (test > 0) {
-    return true;
-  } else {
-    return false;
-  };
-}
-
-function guessMessage() {
-  if (numGuessesLeft >= 0) {
-    var direction = lowerOrHigher();
-    var absolute = Math.abs(playersGuess - winningNumber);
-    var distance = "";
-    if (absolute > 20) {
-      distance = "<span class=\"freezing\" style=\"color:#86C7FF\">Freezing</span>";
-    } else if (absolute > 10) {
-      distance = "<span class=\"cold\" style=\"color:#1D7EFF\">Cold</span>";
-    } else if (absolute < 5) {
-      distance = "<span class=\"hot\" style=\"color:#DC0000\">Hot</span>";
-    } else {
-      distance = "<span class=\"warm\" style=\"color:#FF7A15\">Warm</span>";
-    };
-    $("#info").html("Your Guess is <strong>" + direction.toUpperCase() + "</strong> and <strong>" + distance.toUpperCase() + "</strong>").show();
-  } else {
-    $("#info").html("Sorry! The number was " + winningNumber).show();
-  };
-}
-
-function showLastGuess() {
-  if (prevGuesses.length > 0) {
-    $('#lastGuess').html("Your last guess was: " + prevGuesses[prevGuesses.length - 1]).show();
-  };
-}
+// Ensure that "hintArray" doesn't contain multiple of the same number
 
 function hintDuplicate (array) {
   for (var i = 0; i < array.length; i++) {
@@ -150,7 +145,46 @@ function hintDuplicate (array) {
   };
 }
 
+// Allow the "Player" to Play Again
 
+function playAgain(){
+  gameStart();
+}
+
+// This will happen when the game ends
+
+function gameEnd() {
+  $(".header").hide();
+  $("#guess").hide();
+  $("#info").show();
+  $("#submit").hide();
+  $("#last-guess").hide();
+  $(".during").hide();
+  $(".over").show();
+  $("#helper").hide();
+  $(".ranging").hide();
+}
+
+// Happens on either page load or play again
+
+function gameStart() {
+  prevGuesses = [];
+  numGuessesLeft = 5;
+  hintGiven = false;
+  generateWinningNumber();
+  $(".header").show();
+  $("#guess").show();
+  $("#info").hide();
+  $("#submit").show();
+  $("#last-guess").hide();
+  $(".during").show();
+  $(".over").hide();
+  $("#helper").html("You have " + numGuessesLeft + " more guesses!").show();
+  $(".ranging").show();
+  $("#picture").html("");
+  $("#info").removeClass("winner");
+  $("#info").removeClass("loser");
+}
 
 /* **** Event Listeners/Handlers ****  */
 $(document).ready(function() {
